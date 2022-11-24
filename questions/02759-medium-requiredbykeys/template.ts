61176afd410658d5d602ff48d180f40287bb5748
19302837616 <1115808717@qq.com>
2022-11-24 13:22:16 +0800

    solution: 2759

--- questions/02759-medium-requiredbykeys/template.ts
+++ questions/02759-medium-requiredbykeys/template.ts
@@ -1,1 +1,8 @@
-type RequiredByKeys<T, K> = any
+type RequiredByKeys<T, K extends keyof any = keyof T> = Omit<PickExclude<T, K> & Required<PickAndFilter<T, K>> , never>
+
+type PickExclude<T, K> = Pick<T, Exclude<keyof T, K>>;
+type PickAndFilter<T, K> = Pick<T, K extends keyof T ? K : never>;
+
+type aa = RequiredByKeys<{
+    name? : string
+}, 'name' | 'unknown'>;
