094f2a564bb42c9a13af8c55de061f868d50292a
19302837616 <1115808717@qq.com>
2022-12-19 12:37:33 +0800

    4260

--- questions/04260-medium-nomiwase/template.ts
+++ questions/04260-medium-nomiwase/template.ts
@@ -1,1 +1,9 @@
-type AllCombinations<S> = any
+type AllCombinations<S, Chs extends string = Chars<S>> = 
+    [Chs] extends [never] ? '' 
+    : '' | {[K in Chs]: `${K}${AllCombinations<S, Exclude<Chs, K>>}`}[Chs]
+
+type Chars<S> = S extends `${infer Char}${infer Rest}` ? (Char | Chars<Rest>) : '';
+
+// type TestChars = Chars<'abc'>;
+
+// type a2 = AllCombinations<'abc'>
